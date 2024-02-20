d396d28e4044f3f9bd0689c113cf2b3401f91d25
sunke nithin <nsunke@outlook.com>
2024-02-20 15:09:10 +0530

    initial build scripts

---
+++ configure_hostname.sh
@@ -0,0 +1,10 @@
+.  /tmp/BUILD_SCRIPTS/install_env.sh
+
+echo "******************************************************************************"
+echo "Set Hostname." `date`
+echo "******************************************************************************"
+sudo hostnamectl set-hostname ${HOSTNAME}.${DOMAIN_NAME}
+sudo hostname  ${HOSTNAME}.${DOMAIN_NAME}
+sudo cat > /etc/hostname <<EOF
+${HOSTNAME}.${DOMAIN_NAME}
+EOF
--- os_build.sh
+++ os_build.sh
@@ -1,18 +1,16 @@
 echo "Centos 7 Os build started........"
 
 cd /tmp
-git clone https://github.com/NithinSunke/vsphere_linux_os_build.git
+git clone https://github.com/NithinSunke/BUILD_SCRIPTS.git
 cd /tmp
 ls
-cat  /tmp/vsphere_linux_os_build/config/install_env.sh  > /tmp/CENTOS7_OS_POST_BUILD/install_env.sh
 
-
-.   /tmp/CENTOS7_OS_POST_BUILD/install_env.sh
+.  /tmp/BUILD_SCRIPTS/config/install_env.sh 
 
 echo "installing os packages"
 echo "+++++++++++++++++++++++"
 
-sudo sh  /tmp/CENTOS7_OS_POST_BUILD/install_os_packages.sh
+sudo sh  /tmp/BUILD_SCRIPTS/install_os_packages.sh
 sudo systemctl start nfs-server.service
 sudo systemctl enable nfs-server.service
 sudo systemctl status nfs-server.service
@@ -35,9 +33,9 @@ echo "192.168.0.10:/nfsshare /nfsshare  nfs defaults 0 0"  >> /etc/fstab
 
 echo "setting the hostnames"
 echo "+++++++++++++++++++++++"
-sudo sh /tmp/CENTOS7_OS_POST_BUILD/configure_hostname.sh
+sudo sh /tmp/BUILD_SCRIPTS/configure_hostname.sh
 
-sh /tmp/CENTOS7_OS_POST_BUILD/env.sh
+sh /tmp/BUILD_SCRIPTS/env.sh
 
 echo "echo "InitiatorName=iqn.1994-05.com.scs:`hostname`" > /etc/iscsi/initiatorname.iscsi"
 echo "InitiatorName=iqn.1994-05.com.scs:`hostname`" > /etc/iscsi/initiatorname.iscsi
@@ -46,15 +44,15 @@ echo "=========================================================="
 echo "server build  ${HOSTNAME}.${DOMAIN_NAME} is completed"
 echo "=========================================================="
 
-sudo sh /tmp/CENTOS7_OS_POST_BUILD/iscsi_lun_discover_login.sh
-sudo cp /tmp/CENTOS7_OS_POST_BUILD/iscsi-auto-login.service  /etc/systemd/system/iscsi-auto-login.service
+sudo sh /tmp/BUILD_SCRIPTS/iscsi_lun_discover_login.sh
+sudo cp /tmp/BUILD_SCRIPTS/iscsi-auto-login.service  /etc/systemd/system/iscsi-auto-login.service
 
 
 
 sudo systemctl daemon-reload
 sudo systemctl enable iscsi-auto-login.service
 
-sudo sh /tmp/CENTOS7_OS_POST_BUILD/configure_multipath.sh
-sudo sh /tmp/CENTOS7_OS_POST_BUILD/multipath_names.sh           
+sudo sh /tmp/BUILD_SCRIPTS/configure_multipath.sh
+sudo sh /tmp/BUILD_SCRIPTS/multipath_names.sh           
 
 echo "Centos 7 os build completd........"
