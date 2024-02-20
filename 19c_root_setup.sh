b629449495aa0a95be24e0ab6a855d04646ac5d4
sunke nithin <nsunke@outlook.com>
2024-02-20 19:27:23 +0530

    sd

--- 19c_root_setup.sh
+++ 19c_root_setup.sh
@@ -1,16 +1,16 @@
 . /nfsshare/vagrant/BUILD_SCRIPTS/config/db_install.env
 
-#sh /nfsshare/vagrant/BUILD_SCRIPTS/19c_install_os_packages.sh
-#
-#mkdir -p ${SCRIPTS_DIR}
-#mkdir -p ${SOFTWARE_DIR}
-#mkdir -p ${DATA_DIR}
-#chown -R oracle.oinstall ${SCRIPTS_DIR} /h01 /d01
-#
-#echo "******************************************************************************"
-#echo "Prepare environment and install the software." `date`
-#echo "******************************************************************************"
-#su - oracle -c 'sh /nfsshare/vagrant/BUILD_SCRIPTS/oracle_user_environment_setup.sh'
+sh /nfsshare/vagrant/BUILD_SCRIPTS/19c_install_os_packages.sh
+
+mkdir -p ${SCRIPTS_DIR}
+mkdir -p ${SOFTWARE_DIR}
+mkdir -p ${DATA_DIR}
+chown -R oracle.oinstall ${SCRIPTS_DIR} /h01 /d01
+
+echo "******************************************************************************"
+echo "Prepare environment and install the software." `date`
+echo "******************************************************************************"
+su - oracle -c 'sh /nfsshare/vagrant/BUILD_SCRIPTS/oracle_user_environment_setup.sh'
 su - oracle -c 'sh /nfsshare/vagrant/BUILD_SCRIPTS/19c_oracle_software_installation.sh'
 
 echo "******************************************************************************"
